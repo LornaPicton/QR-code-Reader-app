cc501f4c646d1d7ee5a96457ab83e2f1a20b2536
Lorna <lep21@aber.ac.uk>
2018-04-25 16:42:07 +0100

    final

--- www/app/app.js
+++ www/app/app.js
@@ -2,10 +2,8 @@ var score, aberID, token, name,date, leaderboard
 
 function auth () {
   token = window.localStorage.getItem('token')
-  console.log('on page')
 
   if (!token) {
-    console.log('redirecting')
 
     window.location.href = 'login.html'
   }
@@ -14,20 +12,13 @@ function auth () {
   var student = claims['student'].split(',')
   name = student[0]
   var code = student[4]
-  var role
-  if (code === '[ABSM]' || code === '[MUSM]') {
-    role = 'Staff Member'
-  } else {
-    role = 'Student'
-  }
 }
 
 $(document).ready(function () {
   $('#btn_id').click(function () {
     var aberID = $('#aberID').val()
     var password = $('#password').val()
-    console.log(aberID)
-    console.log(password)
+
     $('#form_id').submit() // Form submission.
     $.ajax({
       type: 'POST',
@@ -40,7 +31,6 @@ $(document).ready(function () {
         demonstrator: true
       },
       success: function (data) {
-        console.log(data)
 
         window.localStorage.setItem('token', data.token)
         window.location.href = 'index.html'
@@ -60,22 +50,10 @@ function logout () {
 }
 
 function scanner () {
-  console.log('in scanner')
   cordova.plugins.barcodeScanner.scan(
     function (result) {
-      alert(
-        'We got a barcode\n' +
-          'Result: ' +
-          result.text +
-          '\n' +
-          'Format: ' +
-          result.format +
-          '\n' +
-          'Cancelled: ' +
-          result.cancelled
-      )
       aberID = result.text
-      QueryDatabase(aberID) // //redirect to student page
+      QueryDatabase(aberID) 
     },
     function (error) {
       alert('Scanning failed: ' + error)
@@ -97,13 +75,10 @@ function scanner () {
 }
 
 function addPoints (points) {
-  console.log('in addPoints')
 
   score += points
 var d = new Date()
-console.log(d)
   date = d.toDateString();
-console.log(date)
   $.ajax({
     type: 'POST',
     url: 'http://svlep21.dcs.aber.ac.uk:3000/leaderboards/' + aberID,
@@ -118,19 +93,19 @@ console.log(date)
     headers: { authorization: 'Bearer ' + token },
 
     success: function (data) {
-      alert('success')
+      alert('Success')
+      points = 0;
       window.location.href = 'index.html'
+      
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
-      
-      // alert("Something wrong happended on the server. Try again..");
       alert('error is ' + errorThrown)
+      points=0;
     }
   })
 }
 
 function QueryDatabase (aberID) {
-  console.log('in queryDatabase')
   $.ajax({
     url: 'http://svlep21.dcs.aber.ac.uk:3000/leaderboards/' + aberID,
     dataType: 'json',
@@ -138,15 +113,10 @@ function QueryDatabase (aberID) {
     headers: { authorization: 'Bearer ' + token },
 
     success: function (data) {
-console.log(data)
+
       if (data.success === false) {
         logout()
       } else {
-   
-      
-
-
-      console.log(data)
       var student =
         'The student ' + data.name + ' has ' + data.score + ' points.'
       score = data.score
@@ -155,7 +125,7 @@ console.log(data)
       document.querySelector('.results').innerHTML = student
     }},
     error: function (XMLHttpRequest, textStatus, errorThrown) {
-      $.mobile.loading('hide', { text: 'Fetching blogs..' })
+      $.mobile.loading('hide')
       // alert("Something wrong happended on the server. Try again..");
       alert('error is ' + errorThrown)
     }
